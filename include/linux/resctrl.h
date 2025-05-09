35852ad42377d3fa5549f385546bd9acf3ceaa42
Chen Yu <yu.c.chen@intel.com>
2025-05-09 23:04:53 +0800

    x86/resctrl: Introduce resctrl_arch_get_config_region() to get the config per region
    Similar to resctrl_arch_get_config(), this newly introduced
    resctrl_arch_get_config_region() get the config based on the
    closid and the region id.
    
    Co-developed-by: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Signed-off-by: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -473,6 +473,8 @@ int resctrl_arch_update_one(struct rdt_resource *r, struct rdt_ctrl_domain *d,
 
 u32 resctrl_arch_get_config(struct rdt_resource *r, struct rdt_ctrl_domain *d,
 			    u32 closid, enum resctrl_conf_type type);
+u32 resctrl_arch_get_config_region(struct rdt_resource *r, struct rdt_ctrl_domain *d,
+				   u32 closid, int region);
 int resctrl_online_ctrl_domain(struct rdt_resource *r, struct rdt_ctrl_domain *d);
 int resctrl_online_mon_domain(struct rdt_resource *r, struct rdt_domain_hdr *hdr);
 void resctrl_offline_ctrl_domain(struct rdt_resource *r, struct rdt_ctrl_domain *d);
